f3d7a5a00edc7d173741c4da676a63d5ccc69adc
SergSentry <debian@debian.com>
2024-02-25 17:06:14 +0300

    lesson1_task2: task complete

---
+++ base/lesson_1/task_2/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    printf("   *   \n");
+    printf("  ***  \n");
+    printf(" ***** \n");
+    printf("*******\n");
+    printf(" HH HH\n");
+    printf(" ZZZZZ");
+    return 0;
+}
