66a20873cf98b41a9972e46b4260ea2194919f22
SergSentry <debian@debian.com>
2024-03-20 21:42:33 +0300

    lesson8_task3: task complete

---
+++ base/lesson_8/task_3/main.c
@@ -0,0 +1,34 @@
+/* e6.c
+ *
+ *  Ввести с клавиатуры массив из 12 элементов, найти среднее арифметическое всех элементов массива
+ *
+ */
+
+#include <stdio.h>
+
+float mean(int source[], int source_size) {
+    float summ = 0;
+    for (int i = 0; i < source_size; i++) {
+        summ += source[i];
+    }
+
+    return summ / source_size;
+}
+
+int main(void) {
+    enum {
+        INPUT_LIMIT = 12
+    };
+
+    int source_data[INPUT_LIMIT] = {0};
+    int i = 0;
+
+    while (i < INPUT_LIMIT) {
+        if (scanf("%d", &source_data[i]) == 1)
+            i++;
+    }
+
+    printf("%.2f", mean(source_data, INPUT_LIMIT));
+
+    return 0;
+}
