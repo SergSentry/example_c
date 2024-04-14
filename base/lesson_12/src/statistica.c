8a5ba0617b750d49e5fef277f1860af4f65de041
SergSentry <debian@debian.com>
2024-04-14 20:46:12 +0300

    statistica: add handle for command line

--- base/lesson_12/src/statistica.c
+++ base/lesson_12/src/statistica.c
@@ -1,10 +1,38 @@
-#include <stdio.h>
+#include <unistd.h>
 
 #include "temp_api.h"
 
-int main(void) {
+int main(int argc, char *argv[]) {
 
-    print_help();
+    int key = 0;
+    char *fileName;
+    int monthNumber = -1;
+    _Bool showHelp = 0;
+
+    while ((key = getopt(argc, argv, "f:m::h::")) != -1) {
+        switch (key) {
+            case 'f':
+                fileName = optarg;
+                break;
+            case 'm':
+                monthNumber = atoi(optarg);
+                break;
+            case 'h':
+                showHelp = 1;
+                break;
+            default:
+                showHelp = 1;
+                break;
+        }
+    }
+
+    if (!showHelp) {
+        if (fileName != NULL) {
+            //open and read file data
+        } else
+            print_help();
+    } else
+        print_help();
 
     return 0;
 }
