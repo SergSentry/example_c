f8328b1c6dce900daf124ddcd3d5e89637596b3f
Serg <coworker@ku.org>
2025-02-11 20:54:36 +0300

    snake: fix checkDirection for any controls

--- advanced/snake/main.c
+++ advanced/snake/main.c
@@ -160,13 +160,18 @@ _Bool isCrash(struct snake_t *head) {
 /*
   Check hitting the tail
 */
-int checkDirection(int32_t dir, int32_t key) {
-    if (KEY_DOWN == key && dir == UP || KEY_UP == key && dir == DOWN || KEY_LEFT == key && dir == RIGHT ||
-        KEY_RIGHT == key && dir == LEFT) {
-        return 0;
-    } else {
-        return 1;
+int checkDirection(struct snake_t *snake, int32_t dir, int32_t key) {
+    for (size_t i = 0; i < CONTROLS; i++) {
+        struct control_buttons ctrl = snake->controls[i];
+
+        if (ctrl.down == key && dir == UP || ctrl.up == key && dir == DOWN || ctrl.left == key && dir == RIGHT ||
+            ctrl.right == key && dir == LEFT) {
+            return 0;
+        }
     }
+    
+
+    return 1;  
 }
 
 int main() {
@@ -186,7 +191,7 @@ int main() {
         goTail(snake);
         timeout(100); // Задержка при отрисовке
 
-        if (checkDirection(snake->direction, key_pressed))
+        if (checkDirection(snake, snake->direction, key_pressed))
             changeDirection(snake, key_pressed);
 
         if (isCrash(snake))
