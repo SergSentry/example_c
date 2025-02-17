82ee2ce96c4cc1fb9b2196f3471ba6a90924bf9a
Serg <coworker@ku.org>
2025-02-17 18:11:52 +0300

    integral: init

---
+++ advanced/integral/src/integral.c
@@ -0,0 +1,19 @@
+/*
+ С заданной точностью ε вычислить площадь плоской фигуры, ограниченной тремя кривыми:
+    1. f(x) = 0.6x + 3
+    2. f(x) = (x − 2)^3 – 1
+    3. f(x) = 3 / x
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <inttypes.h>
+
+/// Основная функция
+/// \param argc Кол-во аргументов
+/// \param argv Массив аргументов
+/// \return Код возврата функции
+int32_t main(int argc, char *argv[]) {
+    return 0;
+}
